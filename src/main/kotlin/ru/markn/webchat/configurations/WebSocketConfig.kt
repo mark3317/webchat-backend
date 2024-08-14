12ab0f4b67cc4d4e4f30f03e7f242c61f4d604e7
mark3317 <mark.nokhrin@gmail.com>
2024-08-15 00:13:48 +0300

    wip: добавил конфигурацию CORS для работы с фронтом

--- src/main/kotlin/ru/markn/webchat/configurations/WebSocketConfig.kt
+++ src/main/kotlin/ru/markn/webchat/configurations/WebSocketConfig.kt
@@ -1,5 +1,6 @@
 package ru.markn.webchat.configurations
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
@@ -9,7 +10,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+class WebSocketConfig(
+    @Value("\${front.url}")
+    private val frontUrl: String
+) : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         config.enableSimpleBroker("/chat", "/invite")
@@ -19,7 +23,7 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry
             .addEndpoint("/ws")
-            .setAllowedOrigins("*")
+            .setAllowedOrigins(frontUrl)
             .withSockJS()
     }
 }
