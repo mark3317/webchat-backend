40e4d8465bc5b1389ea6dfe67d1084301937be6a
mark3317 <mark.nokhrin@gmail.com>
2024-08-21 01:50:44 +0300

    wip: поправил ChatController

--- src/main/kotlin/ru/markn/webchat/dtos/InviteDto.kt
+++ src/main/kotlin/ru/markn/webchat/dtos/InviteDto.kt
@@ -1,12 +1,13 @@
 package ru.markn.webchat.dtos
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
 
 data class InviteDto(
     val chatId: Long?,
     val content: String?,
     @field:NotBlank(message = "Sender name cannot be empty")
     val senderName: String,
-    @field:NotBlank(message = "Recipient names cannot be empty")
+    @field:NotEmpty(message = "Recipient names cannot be empty")
     val recipientNames: List<String>
 )
